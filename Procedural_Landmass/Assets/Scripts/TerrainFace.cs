07a84821600770cbe35719c8f934badd1e80cdc9
Forve <tobias.forve@gmx.de>
2019-08-22 00:11:34 +0200

    UP

---
+++ Procedural_Landmass/Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace : MonoBehaviour
+{
+    private Mesh mesh;
+    private int resolution;
+    private Vector3 localUp;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+    }
+}
